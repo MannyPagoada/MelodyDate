bbd0d4f09c95ff934e3a82e885afe9d043a434a2
Manuel Pagoada <mpagoada@gmail.com>
2024-08-11 16:40:01 -0700

    Basic gui set up. doesnt fully take inputs yet

--- ui/main.py
+++ ui/main.py
@@ -2,7 +2,21 @@ import tkinter as tk
 
 window=tk.Tk()
 
-window.geometry("800x500")
+#window.geometry("800x500")
 window.title("Melody Maker")
+
+label = tk.Label(window, text="MelodyMaker", font=('Arial', 18))
+label.pack(padx=10, pady=10)
+
+label1=tk.Label(window, text="This application is designed to take any date and make an orginal song using machine learning. Give it a try!", font=('Arial', 12),padx=10, pady=10)
+label1.pack()
+
+userInput=tk.Entry(window)
+userInput.pack()
+
+submit=tk.Button(window, text='Submit!', font=('Arial', 8))
+submit.pack(pady=10)
+
+
 
 window.mainloop()
